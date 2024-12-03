b356137c3110617f80e27f2379a62d05a49ca8a6
Yaoee111 <ee.yao@mail.utoronto.ca>
2024-12-03 07:40:32 +0000

    cleaned raw data and get a new dataset

---
+++ scripts/02-clean_data.R
@@ -0,0 +1,34 @@
+#### Preamble ####
+# Purpose: Clean the raw data about apartment evaluation to make it clear for analysis.
+# Author: Yiyi Yao
+# Date: 03 December 2024
+# Contact: ee.yao@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 01-download_data.R
+
+
+#### Workspace setup ####
+library(dplyr)
+
+
+#### Clean raw data ####
+# Read the dataset
+trt_apt <- read.csv("data/01-raw_data/raw_apt_data.csv")
+
+# Filter dataset to keep only relevant wards (11 and 13) and remove rows with NA in Year.Built
+trt_apt_filtered <- trt_apt %>%
+  filter(WARD %in% c("11", "13"))%>%
+  filter(!is.na(YEAR.BUILT))
+
+# Select only relevant features for predicting building condition
+trt_apt_cleaned <- trt_apt_filtered %>%
+  select(RSN, WARD, WARDNAME, YEAR.BUILT, PROPERTY.TYPE, CONFIRMED.STOREYS, 
+         CONFIRMED.UNITS, CURRENT.BUILDING.EVAL.SCORE, PROACTIVE.BUILDING.SCORE, 
+         CURRENT.REACTIVE.SCORE)
+
+# View the cleaned dataset
+head(trt_apt_cleaned)
+
+
+#### Save cleaned data ####
+write.csv(trt_apt_cleaned, "data/02-analysis_data/toronto_apartment_evaluation.csv", row.names = FALSE)
