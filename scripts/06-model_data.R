84072c42f6f4405e14034ec4de8411ac124e5f4e
Yaoee111 <ee.yao@mail.utoronto.ca>
2024-12-12 20:46:16 +0000

    use property type to model

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -16,15 +16,19 @@ trt_apt <- read.csv("data/02-analysis_data/toronto_apartment_evaluation.csv")
 
 # Filter dataset to remove rows with missing values
 trt_apt_cleaned <- trt_apt %>%
-  filter(!is.na(CURRENT.BUILDING.EVAL.SCORE), !is.na(YEAR.BUILT), !is.na(CONFIRMED.UNITS), !is.na(WARD))
+  filter(!is.na(CURRENT.BUILDING.EVAL.SCORE), 
+         !is.na(YEAR.BUILT), 
+         !is.na(CONFIRMED.UNITS), 
+         !is.na(CONFIRMED.STOREYS), 
+         !is.na(PROPERTY.TYPE.CODE))
 
-# Convert WARD to a factor
-trt_apt_cleaned$WARD <- as.factor(trt_apt_cleaned$WARD)
+# Convert PROPERTY_TYPE_CODE to a factor
+trt_apt_cleaned$PROPERTY.TYPE.CODE <- as.factor(trt_apt_cleaned$PROPERTY.TYPE.CODE)
 
 # Fit a Bayesian linear model using rstanarm
-# Predicting CURRENT.BUILDING.EVAL.SCORE based on YEAR.BUILT, CONFIRMED.UNITS, and WARD
+# Predicting CURRENT.BUILDING.EVAL.SCORE based on YEAR.BUILT, PROPERTY_TYPE_CODE, CONFIRMED.STOREYS, and CONFIRMED.UNITS
 bayesian_model <- stan_glm(
-  CURRENT.BUILDING.EVAL.SCORE ~ YEAR.BUILT + CONFIRMED.UNITS + WARD,
+  CURRENT.BUILDING.EVAL.SCORE ~ YEAR.BUILT + PROPERTY.TYPE.CODE + CONFIRMED.STOREYS + CONFIRMED.UNITS,
   data = trt_apt_cleaned,
   family = gaussian(),
   prior = normal(0, 2.5),
@@ -36,4 +40,4 @@ bayesian_model <- stan_glm(
 
 
 #### Save data ####
-saveRDS(bayesian_model,"model/bayesian_model.rds")
+saveRDS(bayesian_model, "model/bayesian_model.rds")
