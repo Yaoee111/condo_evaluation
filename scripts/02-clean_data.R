edb7587930e94a5323c05345f5efbbcd1be60404
Yaoee111 <ee.yao@mail.utoronto.ca>
2024-12-12 20:11:01 +0000

    modify cleaned data: delete ward 11 and add property code

--- scripts/02-clean_data.R
+++ scripts/02-clean_data.R
@@ -15,16 +15,24 @@ library(dplyr)
 # Read the dataset
 trt_apt <- read.csv("data/01-raw_data/raw_apt_data.csv")
 
-# Filter dataset to keep only relevant wards (11 and 13) and remove rows with NA in Year.Built
+# Filter dataset to keep only relevant wards (13) and remove rows with NA in Year.Built
 trt_apt_filtered <- trt_apt %>%
-  filter(WARD %in% c("11", "13"))%>%
+  filter(WARD %in% c("13"))%>%
   filter(!is.na(YEAR.BUILT))
 
 # Select only relevant features for predicting building condition
-trt_apt_cleaned <- trt_apt_filtered %>%
+trt_apt_selected <- trt_apt_filtered %>%
   select(RSN, WARD, WARDNAME, YEAR.BUILT, PROPERTY.TYPE, CONFIRMED.STOREYS, 
-         CONFIRMED.UNITS, CURRENT.BUILDING.EVAL.SCORE, PROACTIVE.BUILDING.SCORE, 
-         CURRENT.REACTIVE.SCORE)
+         CONFIRMED.UNITS, CURRENT.BUILDING.EVAL.SCORE)
+
+# Create a new column PROPERTY_TYPE_CODE with categorization
+trt_apt_cleaned <- trt_apt_selected %>%
+  mutate(PROPERTY_TYPE_CODE = case_when(
+    PROPERTY.TYPE == "PRIVATE" ~ 1,
+    PROPERTY.TYPE == "SOCIAL HOUSING" ~ 2,
+    PROPERTY.TYPE == "TCHC" ~ 3,
+    TRUE ~ 0  
+  ))
 
 # View the cleaned dataset
 head(trt_apt_cleaned)
